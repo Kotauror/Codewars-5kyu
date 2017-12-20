b33b55f2115455b33428cd80339d944d64cf6eaf
kotpsot <zygmunt.justyna@gmail.com>
2017-12-20 21:40:15 +0000

    add additional solution to valid parentheses kata

--- valid_parentheses.rb
+++ valid_parentheses.rb
@@ -4,7 +4,7 @@ def valid_parentheses(string)
   alpha = ("a".."z").to_a
 
   string2 = string.chars.map{|x|
-    if alpha.include?(x) then 
+    if alpha.include?(x) then
       ""
     else
       x
@@ -19,4 +19,23 @@ def valid_parentheses(string)
 
   string2.strip.empty? ? true : false
 
+end
+
+
+#------ ANOTHER SOLUTION
+
+def valid_parentheses(string)
+  counter=0
+  string.chars{|x|
+    if x=="(" then
+      counter += 1
+    elsif x==")" then
+      counter -= 1
+    end
+    if counter < 0 then #we have closed more parenthesis than we've opened // checks )
+      return false #we cannot wait till the end since we can open another par... and counter will be increase and give a no no  result
+    end
+  }
+  #the line below works for such cases: ()( , (() - counter is > 0 so it doesn't return false
+  return counter == 0 #it means return true if counter indeed equals 0  //checks (  [ now we want hom many ( we have opened so can check if every has pair)
 end
