9c88c3a687a103df4e4bef04bbc9d33dba4c6358
kotpsot <zygmunt.justyna@gmail.com>
2018-04-17 20:24:38 +0100

    Add additional solutions to scramble.

--- scramble.rb
+++ scramble.rb
@@ -14,3 +14,50 @@ def scramble(s1,s2)
   return lon == s2_d
 
 end
+
+
+def scramble(s1,s2)
+
+  hash = {}
+
+  s1.chars { |letter|
+    if hash[letter] != nil then
+      hash[letter] += 1
+    else
+      hash[letter] = 1
+    end
+  }
+
+  s2.chars { |letter|
+    if hash[letter] != 0 && hash[letter] != nil then
+       hash[letter] -= 1
+    else
+      return false
+    end
+  }
+
+  return true
+
+end
+
+
+def scramble(s1,s2)
+
+  hash = {}
+  hash.default = 0
+
+  s1.chars { |letter|
+    hash[letter] += 1
+  }
+
+  s2.chars { |letter|
+    if hash[letter] != 0 then
+       hash[letter] -= 1
+    else
+      return false
+    end
+  }
+
+  return true
+
+end
